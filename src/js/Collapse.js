d37bba28ff80f11757ba2a2be977fc75753435ea
Elena Kochegarova <lelet@ngs.ru>
2023-04-01 08:40:27 +0700

    finished

--- src/js/Collapse.js
+++ src/js/Collapse.js
@@ -1,21 +1,47 @@
+/* eslint-disable class-methods-use-this */
 export default class Collapse {
   constructor() {
     this.elem = this.create();
+    this.button = this.elem.querySelector('.collapse__button');
+    this.content = this.elem.querySelector('.collapse__content');
+
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  static get markup() {
+    return `
+      <button class="collapse__button">Collapse</button>
+      <p class="collapse__content content_closed">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ad itaque, enim numquam quas fuga dolorem hic dolorum sunt placeat, reprehenderit corporis ducimus? Ut deleniti fuga eum ipsum error numquam!
+      </p>
+    `;
   }
 
   init() {
-
+    document.body.append(this.elem);
+    this.button.addEventListener('click', this.onButtonClick);
   }
 
   create() {
+    const collapse = document.createElement('section');
+    collapse.classList.add('collapse');
 
+    collapse.insertAdjacentHTML('beforeend', Collapse.markup);
+
+    return collapse;
   }
 
-  show() {
-
+  onButtonClick() {
+    this.addAnimation();
   }
 
-  hide() {
-
+  addAnimation() {
+    if (this.content.classList.contains('content_opened')) {
+      this.content.classList.remove('content_opened');
+      this.content.classList.add('content_closed');
+    } else {
+      this.content.classList.remove('content_closed');
+      this.content.classList.add('content_opened');
+    }
   }
 }
